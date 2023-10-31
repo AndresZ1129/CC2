5008b4b1894d88f10fa05390a328dc98f5e643d5
AndresZ1129 <145486662+AndresZ1129@users.noreply.github.com>
2023-10-31 18:01:14 -0400

    Update restaurant.php

--- restaurant.php
+++ restaurant.php
@@ -6,7 +6,18 @@
 
     <body>
         <?php
-            
+           $formFields = array(
+            'Fogo de Chão' => '40.50',
+            'Aviva by Kameel' => '18.00',
+            'Bones Restaurant' => '65.00',
+            'Umi Sushi Buckhead' => '40.50',
+            'Fandangles' => '30.00',
+            'Capital Grille' => '60.50',
+            'Canoe' => '35.50',
+            'One Flew South' => '21.00',
+            'Fox Bros. BBQ' => '15.05',
+            'South City Kitchen Midtown' => '29.00',
+            );
         ?>
     </body>
 </html>
