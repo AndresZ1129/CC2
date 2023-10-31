db1280187f79babda4c666ca18c40ec147617e2e
AndresZ1129 <145486662+AndresZ1129@users.noreply.github.com>
2023-10-31 17:53:14 -0400

    Update restaurant.php

---
+++ restaurant.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Restaurant</title>
+        <link rel="stylesheet" href="restaurant.css">
+    </head>
+
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
